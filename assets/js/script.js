f614bbd6feb01dc3f1266bac0fe09dfde000c5f1
Poney <you@example.com>
2022-03-21 22:39:38 +0100

    Responsive terminé, images webp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,29 @@
 const backToTopButton = document.querySelector("#back-to-top-btn");
 
-window.addEventListener("scroll", scrollFunction);
+window.addEventListener("scroll", scroll);
 
-function scrollFunction() {
+function scroll() {
   if (window.pageYOffset > 300) { // Montrer Bouton
-    if(!backToTopButton.classList.contains("btnEntrance")) {
-      backToTopButton.classList.remove("btnExit");
-      backToTopButton.classList.add("btnEntrance");
+    if(!backToTopButton.classList.contains("btnEntree")) {
+      backToTopButton.classList.remove("btnSortie");
+      backToTopButton.classList.add("btnEntree");
       backToTopButton.style.display = "block";
     }
   }
   else { // Cacher Bouton
-    if(backToTopButton.classList.contains("btnEntrance")) {
-      backToTopButton.classList.remove("btnEntrance");
-      backToTopButton.classList.add("btnExit");
+    if(backToTopButton.classList.contains("btnEntree")) {
+      backToTopButton.classList.remove("btnEntree");
+      backToTopButton.classList.add("btnSortie");
       setTimeout(function() {
         backToTopButton.style.display = "none";
       }, 300);
     }
   }
+}
+
+backToTopButton.addEventListener("click", backtotop);
+
+function backtotop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
